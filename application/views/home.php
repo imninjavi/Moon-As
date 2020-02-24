f941bbb583b0cf7a81373c709ba89afeea05793c
ninjavi98 <ninja.vi98@gmail.com>
2020-02-24 19:50:02 +0700

    Fix home display

--- application/views/home.php
+++ application/views/home.php
@@ -83,71 +83,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <span class="icon-bar"></span>
                         </button>
                         <div class="logo-normal">
-                            <a class="navbar-brand" href="index.php"><img src="<?php echo base_url('assets/images/logo.png')?>" alt=""></a>
+                            <a class="navbar-brand" href="index.php"><img src="<?php echo base_url('assets/images/logo3.png')?>" alt=""></a>
                         </div>
                     </div>
 
                     <div id="navbar" class="navbar-collapse collapse">
                         <ul class="nav navbar-nav navbar-right">
-                            <li><a href="index.html">Home</a></li>
-                            <li class="dropdown yamm-fw yamm-half"><a href="#" data-toggle="dropdown" class="dropdown-toggle active">Mega Menu <b class="fa fa-angle-down"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <div class="yamm-content clearfix">
-                                            <div class="row-fluid">
-                                                <div class="col-md-6 col-sm-6">
-                                                    <h4>Course Pages</h4>
-                                                    <ul>
-                                                        <li><a href="#">Courses Name 01</a></li>
-                                                        <li><a href="#">Courses Name 02</a></li>
-                                                        <li><a href="#">Courses Name 03</a></li>
-                                                        <li><a href="#">Courses Name 04</a></li>
-                                                        <li><a href="#">Courses Name 05</a></li>
-                                                        <li><a href="#">Courses Name 06</a></li>
-                                                        <li><a href="#">Courses Name 07</a></li>
-                                                        <li><a href="#">Courses Name 08</a></li>
-                                                        <li><a href="#">Courses Name 09</a></li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-md-6 col-sm-6">
-                                                    <div class="menu-widget text-center">
-                                                        <div class="image-wrap entry">
-                                                            <img src="<?php echo base_url('assets/upload/course_01.jpg')?>" alt="" class="img-responsive">
-                                                            <div class="magnifier">
-                                                                 <a href="#" title=""><i class="flaticon-add"></i></a>
-                                                            </div>
-                                                        </div><!-- end image-wrap -->
-                                                        <h5><a href="#">Learning Bootstrap Framework</a></h5>
-                                                        <small>$22.00</small>
-                                                        <a href="#" class="menu-button">View Course</a>
-                                                    </div><!-- end widget -->
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="events.html">Events</a></li>
-                            <li class="dropdown hassubmenu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Shop <span class="fa fa-angle-down"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="shop.html">Shop Layout</a></li>
-                                    <li><a href="shop-single.html">Shop Single</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown hassubmenu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Blog <span class="fa fa-angle-down"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="blog.html">Blog Right Sidebar</a></li>
-                                    <li><a href="blog-1.html">Blog Left Sidebar</a></li>
-                                    <li><a href="blog-2.html">Blog Grid Sidebar</a></li>
-                                    <li><a href="blog-3.html">Blog Grid Fullwidth</a></li>
-                                    <li><a href="blog-single.html">Blog Single</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="index.php">Beranda</a></li>
+                            <li><a href="<?php echo base_url('registrasi') ?>">Registrasi</a></li>
                             <li><a href="page-contact.html">Contact</a></li>
-                            <li class="iconitem"><a href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-search"></i></a></li>
-                            <li class="iconitem"><a class="shopicon" href="shop-cart.html"><i class="fa fa-shopping-basket"></i> &nbsp;(0)</a></li>
                         </ul>
                     </div>
                 </nav><!-- end navbar -->
@@ -219,7 +163,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <small>Pendaftaran akan dibuka 2 minggu sebelum pelaksanaan acara.</small>
                     <div class="btn-wrapper">
                         <div class="text-center">
-                            <a href="#" class="btn btn-primary wow slideInLeft">Read More</a> &nbsp;&nbsp;&nbsp;<a href="#" class="btn btn-default wow slideInRight">Buy Now</a>
+                            <a href="#" class="btn btn-primary wow slideInLeft">Download  <b class="fa fa-download"></b></a> &nbsp;&nbsp;&nbsp;<a href="registrasi.php" class="btn btn-default wow slideInRight">Registrasi</a>
                         </div>
                     </div><!-- end row -->
                 </div>
@@ -359,7 +303,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="clearfix">
                     <div class="pull-left">
                         <div class="cop-logo">
-                            <a href="#"><img src="<?php echo base_url('assets/images/logo.png')?>" alt=""></a>
+                            <a href="#"><img src="<?php echo base_url('assets/images/logo2.png')?>" alt=""></a>
                         </div>
                     </div>
 
@@ -392,8 +336,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     $('document').ready(function() {
         'use strict';
 
-        var start = Date.parse('Wed Feb 20 19:20:44 +0000 2020')/1000;
-        var end = Date.parse('Wed Mar 28 19:20:44 +0000 2020')/1000;
+        var start = Date.parse('Wed Feb 20 00:00:00 +0700 2020')/1000;
+        var end = Date.parse('Wed Mar 27 00:00:00 +0700 2020')/1000;
         var now = new Date().getTime() / 1000;
         
     	$('.countdown').final_countdown({
